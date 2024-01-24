1640cafd0405ccf390cfe5d1725398722d2a1031
wlangera <ward.langeraert@inbo.be>
2024-01-24 21:00:19 +0100

    coding style

--- source/R/calc_parcel_size.R
+++ source/R/calc_parcel_size.R
@@ -1,12 +1,12 @@
 calc_parcel_size <- function(layer) {
-  require(dplyr)
-  require(sf)
+  require("dplyr")
+  require("sf")
 
   parcels_grouped <- layer %>%
-    filter(!is.na(GWSNAM_H)) %>%
+    filter(!is.na(.data$GWSNAM_H)) %>%
 
     # Join neighbouring polygons by main crop
-    group_by(GWSNAM_H, GWSGRPH_LB) %>%
+    group_by(.data$GWSNAM_H, .data$GWSGRPH_LB) %>%
     summarise() %>%
     st_cast("MULTIPOLYGON") %>%
     st_cast("POLYGON") %>%
@@ -17,9 +17,9 @@ calc_parcel_size <- function(layer) {
     rownames_to_column(var = "ID") %>%
 
     # Calculate area
-    mutate(area_m2 = st_area(geometry),
-           area_m2 = units::drop_units(area_m2),
-           area_ha = area_m2 / 10000)
+    mutate(area_m2 = st_area(.data$geometry),
+           area_m2 = units::drop_units(.data$area_m2),
+           area_ha = .data$area_m2 / 10000)
 
   return(out_sf)
 }
