ab57bdc2c15cc79b5ff01cf14d5d48b37521dc73
wlangera <ward.langeraert@inbo.be>
2024-01-24 18:13:12 +0100

    keep grouping variable

--- source/R/calc_parcel_size.R
+++ source/R/calc_parcel_size.R
@@ -6,7 +6,7 @@ calc_parcel_size <- function(layer) {
     filter(!is.na(GWSNAM_H)) %>%
 
     # Join neighbouring polygons by main crop
-    group_by(GWSNAM_H) %>%
+    group_by(GWSNAM_H, GWSGRPH_LB) %>%
     summarise() %>%
     st_cast("MULTIPOLYGON") %>%
     st_cast("POLYGON") %>%
